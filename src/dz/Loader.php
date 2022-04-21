8140760839923ecefea0414056640d8bcd07b9b2
dhnnz <104043390+dhnnz@users.noreply.github.com>
2022-04-21 14:54:11 +0700

    Update Loader.php

--- src/dz/Loader.php
+++ src/dz/Loader.php
@@ -1,2 +1,40 @@
 <?php
+
+namespace dz;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class Loader extends PluginBase
+{
+  /**
+   * onEnable():
+   */
+  public function onEnable(): void
+  {
+    $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
+    $this->saveResource("config.yml");
+    $this->getServer()->info("plugin CustomJoinMessage has enable");
+  }
+  /**
+   * 
+   * PlayerJoinEvent()
+   * 
+   */
+  public function onJoin(\pocketmine\event\player\PlayerJoinEvent $event)
+  {
+    $p = $event->getPlayer();
+    $event->setJoinMessage($this->convertString($this->config->get("join-message"), $p));
+  }
+  
+  /**
+   * convert to String
+   * data Config
+   */
+  public function convertString(string $string, Player $p)
+  {
+    $string = str_replace("{player}", $p->getName(), $string);
+    return $string;
+  }
+}
 ?>
